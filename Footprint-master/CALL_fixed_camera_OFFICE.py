c93b6e7317f7e6c62ede49cef88430041a35d22f
thomasong0204 <thorth_ong@yahoo.com.sg>
2018-01-11 17:22:57 +0800

    update for today. update the recreating the footprint

--- Footprint-master/CALL_fixed_camera_OFFICE.py
+++ Footprint-master/CALL_fixed_camera_OFFICE.py
@@ -9,11 +9,116 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import urllib2,psycopg2,math
+
+import urllib2
+import math
+import psycopg2
 from shapely import wkb
 from shapely.geometry import Polygon
 from shapely.affinity import rotate
+from shapely.wkt import dump
 
+def breakMultiPoly():
+
+   return
+
+
+def ReformBackShape(bearingRad,PolyRotate,x2,x1,y1):
+## this is needed to repair the geometry. After clipping, the outline of the feature are changed to more than 4 points
+## thus, a need to repair to ensure the geometry are back to 4 points.
+   rotateback = 2*math.pi-bearingRad
+   PolyRotate = rotate(PolyRotate,rotateback,cam_geom,use_radians=True)
+   OldPolyBB = PolyRotate.bounds
+   Minx = OldPolyBB[0]
+   Maxx = OldPolyBB[2]
+   Maxy = OldPolyBB[3]
+   print "bbox: "+str(OldPolyBB[0])
+        ## Coordinate in this order.
+        ## x4,y2 ------------ x3,y2
+        ##   |    (x4+x3)/2    |
+        ##    |              |
+        ## x2,y1 ---------- x1,y1
+
+   ReformPoly = Polygon(((Minx,Maxy),(Maxx,Maxy),(x1,y1),(x2,y1)))
+
+   return PolyRotate
+
+
+def CreateFixedFootprint(view_angle, sensorWidth, sensorHeight, focusLength, bearing, cameraX, cameraY):
+    viewingAngle = (90-float(view_angle)) * math.pi /180
+    FOVWidthAngle = 2*math.atan (sensorWidth/(2*focusLength))
+    FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
+    bearingRad = (360-bearing)*2*math.pi/360
+    cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
+    cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
+    ##  calculate the left and right
+    hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)))
+    trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower
+    hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)))
+    trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop
+    if trapTop < 0:
+       trapTop = abs(trapTop)
+    if trapBottom < 0:
+       trapBottom = abs(trapBottom)
+    if cameraToTop < 0:
+       cameraToTop = abs(cameraToTop)
+    if cameraToBottom < 0:
+       cameraToBottom = abs(cameraToBottom)
+        ## Coordinate in this order.
+        ## x4,y2 ------------ x3,y2
+        ##   |    (x4+x3)/2    |
+        ##    |              |
+        ## x2,y1 ---------- x1,y1
+    x1 = float(cameraX + trapBottom)
+    x2 = float(cameraX - trapBottom)
+    x3 = float(cameraX + trapTop)
+    x4 = float(cameraX - trapTop)
+    y1 = float(cameraY + cameraToBottom)
+    y2 = float(cameraY + cameraToTop)
+    polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
+    PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
+    print "before==> "+ str(PolyRotate)
+    ## operation to join floorplan with the floor print
+    PolyRotate = floorplanPoly.intersection(PolyRotate).convex_hull
+    PolyRotateB = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1) ## Rotate and adjust the polygon back to 4 corner
+    print "polyboundary=> "+str(PolyRotateB)
+
+    polyText = PolyRotateB.wkt
+    return polyText
+
+
+
+def CreateDOMEFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY):
+    viewingAngle = (90-float(view_angle)) * math.pi /180
+    FOVWidthAngle = 2*math.atan (sensorWidth/(2*focusLength))
+    FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
+    bearingRad = (360-bearing)*2*math.pi/360
+    cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
+    cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
+    ##  calculate the left and right
+    hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)))
+    trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower
+    hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)))
+    trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop
+        ## Coordinate in this order.
+        ## x4,y2 ------------ x3,y2
+        ##   |    (x4+x3)/2    |
+        ##    |              |
+        ## x2,y1 ---------- x1,y1
+    x1 = float(cameraX + trapBottom)
+    x2 = float(cameraX - trapBottom)
+    x3 = float(cameraX + trapTop)
+    x4 = float(cameraX - trapTop)
+    y1 = float(cameraY + cameraToBottom)
+    y2 = float(cameraY + cameraToTop)
+    polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
+    PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
+    print "before==> "+ str(PolyRotate)
+    ## operation to join floorplan with the floor print
+    PolyRotate = floorplanPoly.intersection(PolyRotate).convex_hull
+    PolyRotateB = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1) ## Rotate and adjust the polygon back to 4 corner
+    polyText = PolyRotate.wkt
+    return polyText
 
 db="osi_social_db"
 dbuser="postgres"
@@ -30,8 +135,11 @@ db = psycopg2.connect(database=db, user=dbuser, password=dbpassword, host=dbhost
 
 ## extract the floor plan to compare
 cursor = db.cursor()
-cursor.execute("""select * from "osi_office" where name = 'floor';""") ## <== get the floor from the floor plan
-floorPlan = cursor.fetchall()
+cursor.execute("""select geom from "osi_office" where name = 'floor';""") ## <== get the floor from the floor plan
+floorPlan, = cursor.fetchone()
+floorplanPoly = wkb.loads(floorPlan,hex=True)  ## convert the floor geom
+print "floorplanPoly ==> "+ str(floorplanPoly)
+
 
 ## extract the camera detail
 cursor = db.cursor()
@@ -59,61 +167,10 @@ for camera in CameraList:
     if cameraType == "FIXED":
 ##        viewingAngle = float(view_angle) * math.pi /180
         ## change the viewing angle to |\ <= viewing angle
-        viewingAngle = (90-float(view_angle)) * math.pi /180
-        FOVWidthAngle = 2*math.atan (sensorWidth/(2*focusLength))
-        FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
-        bearingRad = (360-bearing)*2*math.pi/360
-
-##        if bearing < 180:
-##            bearingRad = (bearing)*2*math.pi/360
-##        else:
-##            bearingRad = (bearing+180)*2*math.pi/360
-        print str(camera_UID)+": "+str(bearingRad)
-
-
-        cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
-        cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
-
-##       calculate the left and right
-        hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)));
-        trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower;
-
-        hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)));
-        trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop;
-        if trapTop < 0:
-            trapTop = abs(trapTop)
-        if trapBottom < 0:
-            trapBottom = abs(trapBottom)
-
-        if cameraToTop < 0:
-            cameraToTop = abs(cameraToTop)
-        if cameraToBottom < 0:
-            cameraToBottom = abs(cameraToBottom)
-
-
-
-
-        ## Coordinate in this order.
-        ## x4,y2 ------------ x3,y2
-        ##   |    (x4+x3)/2    |
-        ##    |              |
-        ## x2,y1 ---------- x1,y1
-        x1 = float(cameraX + trapBottom);
-        x2 = float(cameraX - trapBottom);
-        x3 = float(cameraX + trapTop);
-        x4 = float(cameraX - trapTop);
-
-        y1 = float(cameraY + cameraToBottom);
-        y2 = float(cameraY + cameraToTop);
-
-        polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
-        PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
-
-        polyText = PolyRotate.wkt
+        polyText = CreateFixedFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
         polystarement = "ST_GeomFromText('"+polyText+"',3857)"
 
-##        print sqlStatement % (camera_UID,polystarement)
-
+##      print sqlStatement % (camera_UID,polystarement)
         CheckFootprintExist = """Select count(uid) from office_footprint where uid = '%s';"""
         InsertStatement = """insert into office_footprint(uid,geom) values('%s',%s)"""
         UpdateStatement = """update office_footprint
@@ -126,53 +183,17 @@ for camera in CameraList:
             ## Insert row
             cursor.execute(InsertStatement % (camera_UID,polystarement))
             db.commit()
+            pass
         else:
             ## Update row
             cursor.execute(UpdateStatement % (polystarement,camera_UID))
             db.commit()
-            print "Update row"
-            print "*"*50
+            pass
 
     elif cameraType == "DOME":
-        viewingAngle = float(view_angle) * math.pi /180
-        ## change the viewing angle to |\ <= viewing angle
-##        viewingAngle = (90-float(view_angle)) * math.pi /180
-        FOVWidthAngle = 2*math.atan (sensorWidth/(2*focusLength))
-        FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
-        bearingRad = (360-bearing)*2*math.pi/360
 
-        cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
-        cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
-
-##       calculate the left and right
-        hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)));
-        trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower;
-
-        hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)));
-        trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop;
-
-        ## Coordinate in this order.
-        ## x4,y2 ------------ x3,y2
-        ##   |    (x4+x3)/2    |
-        ##    |              |
-        ## x2,y1 ---------- x1,y1
-        x1 = float(cameraX + trapBottom);
-        x2 = float(cameraX - trapBottom);
-        x3 = float(cameraX + trapTop);
-        x4 = float(cameraX - trapTop);
-
-        y1 = float(cameraY + cameraToBottom);
-        y2 = float(cameraY + cameraToTop);
-
-
-
-        polyabc = Polygon(((x4,y2),(x3,y2),(x2,y1),(x1,y1)))
-        PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
-
-        polyText = PolyRotate.wkt
-
-
-        polystarement = "ST_GeomFromText('"+polyText+"',3857)"
+        polyTextdOME = CreateDOMEFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
+        polystarement = "ST_GeomFromText('"+polyTextdOME+"',3857)"
 
 
 ##        print sqlStatement % (camera_UID,polystarement)
@@ -189,14 +210,12 @@ for camera in CameraList:
             ## Insert row
             cursor.execute(InsertStatement % (camera_UID,polystarement))
             db.commit()
+            pass
         else:
             ## Update row
-            print("==> "+UpdateStatement)
-            print UpdateStatement % (polystarement,camera_UID)
             cursor.execute(UpdateStatement % (polystarement,camera_UID))
             db.commit()
-            print "*** Update dome row"
-            print "*"*30
+            pass
     else:
         pass
 
