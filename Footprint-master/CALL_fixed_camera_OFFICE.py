5124623c35227d70bec9755fc7f465c05e22eafa
thomasong0204 <thorth_ong@yahoo.com.sg>
2018-01-22 09:28:19 +0800

    work done on friday. transformation parameter are now stored in db

--- Footprint-master/CALL_fixed_camera_OFFICE.py
+++ Footprint-master/CALL_fixed_camera_OFFICE.py
@@ -26,79 +26,106 @@ def breakMultiPoly():
    pass
 
 
-def ReformBackShape(bearingRad,PolyRotate,x2,x1,y1):
+def ReformBackShape(bearingRad,PolyRotate,x2,x1,y1,cam_height,cameraToTop,cameraY):
 ## this is needed to repair the geometry. After clipping, the outline of the feature are changed to more than 4 points
 ## thus, a need to repair to ensure the geometry are back to 4 points.
-	rotateback = 2*math.pi-bearingRad
-	PolyRotate = rotate(PolyRotate,rotateback,cam_geom,use_radians=True)
-	OldPolyBB = PolyRotate.bounds
-	Minx = OldPolyBB[0]
-	Maxx = OldPolyBB[2]
-	Maxy = OldPolyBB[3]
-   ## Coordinate in this order.
-   ## x4,y2 ------------ x3,y2
-   ##   |    (x4+x3)/2    |
-   ##    |              |
-   ## x2,y1 ---------- x1,y1
-	## compare to check if there's any clipping in the x1 positon.
-	## update if there is.
-	if Minx > x2:
-		newx2 = Minx
-	else:
-		newx2 = x2
+    rotateback = 2*math.pi-bearingRad
+    PolyRotate = rotate(PolyRotate,rotateback,cam_geom,use_radians=True)
+    OldPolyBB = PolyRotate.bounds
+    Minx = OldPolyBB[0]
+    Maxx = OldPolyBB[2]
+    Maxy = OldPolyBB[3]
+    ## Coordinate in this order.
+    ## x4,y2 ------------ x3,y2
+    ##   |    (x4+x3)/2    |
+    ##    |              |
+    ## x2,y1 ---------- x1,y1
+    ## compare to check if there's any clipping in the x1 positon.
+    ## update if there is.
+    if Minx > x2:
+        newx2 = Minx
+    else:
+        newx2 = x2
 
-	if Maxx < x1:
-		newx1 = Maxx
-	else:
-		newx1 = x1
-	## recreate the polygon
-	ReformPoly = Polygon(((Minx,Maxy),(Maxx,Maxy),(newx1,y1),(newx2,y1)))
+    if Maxx < x1:
+        newx1 = Maxx
+    else:
+        newx1 = x1
+    
+    ## calculate the height
+    # Near field
+    d2 = cameraToTop- (y1- cameraY)
+    # print "d2: "+str(d2)
+    near =  round(cam_height*d2/cameraToTop,2)
+    if near < 0:
+        near = abs(near)
+    if near > cam_height:
+        near = cam_height
+
+    # print "near==> "+str(near)+","+str(cam_height)
+    ## calculate the height
+    # far field
+    d1 = cameraToTop-(Maxy- cameraY)
+    # print "d1: "+str(d1)
+    far =  round(cam_height*d1/cameraToTop,2)
+    # print "far==> "+str(far)+","+str(cam_height)
+    ## calculate if camera view is far/near view
+    angle = math.tan(cam_height/cameraToTop)
+    convertAngle = 90- angle/math.pi*180
+    d1 = cameraToTop-(Maxy- cameraY)
+    far =  round(cam_height*d1/cameraToTop,2)
+
+
+    ## recreate the polygon
+    ReformPoly = Polygon(((Minx,Maxy),(Maxx,Maxy),(newx1,y1),(newx2,y1)))
 
     ## rotation to be done in javascript
 
-	return ReformPoly
+    return (ReformPoly,far,near)
 
 
 def CreateFixedFootprint(view_angle, sensorWidth, sensorHeight, focusLength, bearing, cameraX, cameraY):
-	viewingAngle = (90-float(view_angle)) * math.pi /180
-	FOVWidthAngle = 2*math.atan(sensorWidth/(2*focusLength))
-	FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
-	bearingRad = (360-bearing)*2*math.pi/360
-	cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
-	cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
-	##  calculate the left and right
-	hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)))
-	trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower
-	hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)))
-	trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop
-	if trapTop < 0:
-		trapTop = abs(trapTop)
-	if trapBottom < 0:
-		trapBottom = abs(trapBottom)
-	if cameraToTop < 0:
-		cameraToTop = abs(cameraToTop)
-	if cameraToBottom < 0:
-		cameraToBottom = abs(cameraToBottom)
+    viewingAngle = (90-float(view_angle)) * math.pi /180
+    FOVWidthAngle = 2*math.atan(sensorWidth/(2*focusLength))
+    FOVHeightAngle = 2*math.atan(sensorHeight/(2*focusLength))
+    bearingRad = (360-bearing)*2*math.pi/360
+    cameraToBottom = cam_height*math.tan(viewingAngle-0.5*FOVWidthAngle)
+    cameraToTop = cam_height*math.tan(viewingAngle+0.5*FOVWidthAngle)
+    ##  calculate the left and right
+    hypoLower = cam_height/(math.cos(viewingAngle-(0.5*FOVHeightAngle)))
+    trapBottom = math.tan(0.5*FOVHeightAngle)*hypoLower
+    hypoTop = cam_height/(math.cos(viewingAngle+(0.5*FOVHeightAngle)))
+    trapTop = math.tan(0.5*FOVHeightAngle)*hypoTop
+    if trapTop < 0:
+        trapTop = abs(trapTop)
+    if trapBottom < 0:
+        trapBottom = abs(trapBottom)
+    if cameraToTop < 0:
+        cameraToTop = abs(cameraToTop)
+    if cameraToBottom < 0:
+        cameraToBottom = abs(cameraToBottom)
         ## Coordinate in this order.
         ## x4,y2 ------------ x3,y2
         ##   |    (x4+x3)/2    |
         ##    |              |
         ## x2,y1 ---------- x1,y1
-	x1 = float(cameraX + trapBottom)
-	x2 = float(cameraX - trapBottom)
-	x3 = float(cameraX + trapTop)
-	x4 = float(cameraX - trapTop)
-	y1 = float(cameraY + cameraToBottom)
-	y2 = float(cameraY + cameraToTop)
-	polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
-	PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
+    x1 = float(cameraX + trapBottom)
+    x2 = float(cameraX - trapBottom)
+    x3 = float(cameraX + trapTop)
+    x4 = float(cameraX - trapTop)
+    y1 = float(cameraY + cameraToBottom)
+    y2 = float(cameraY + cameraToTop)
 
-	## operation to join floorplan with the floor print
-	PolyRotate = floorplanPoly.intersection(PolyRotate).convex_hull
-	PolyRotateB = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1) ## Rotate and adjust the polygon back to 4 corner
+    ## Calculate z at x2.
 
-	polyText = PolyRotateB.wkt
-	return polyText
+    polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
+    PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
+
+    ## operation to join floorplan with the floor print
+    PolyRotate = floorplanPoly.intersection(PolyRotate).convex_hull
+    PolyRotateB,y1height,y2height = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1,cam_height,cameraToTop,cameraY) ## Rotate and adjust the polygon back to 4 corner
+    polyText = PolyRotateB.wkt
+    return (polyText,y1height,y2height)
 
 
 
@@ -125,14 +152,16 @@ def CreateDOMEFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,
     x4 = float(cameraX - trapTop)
     y1 = float(cameraY + cameraToBottom)
     y2 = float(cameraY + cameraToTop)
+
     polyabc = Polygon(((x4,y2),(x3,y2),(x1,y1),(x2,y1)))
     PolyRotate = rotate(polyabc,bearingRad,cam_geom,use_radians=True)
 
     ## operation to join floorplan with the floor print
     PolyRotate = floorplanPoly.intersection(PolyRotate).convex_hull
-    PolyRotateB = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1) ## Rotate and adjust the polygon back to 4 corner
+    PolyRotateB,y1height,y2height = ReformBackShape(bearingRad,PolyRotate,x2,x1,y1,cam_height,cameraToTop,cameraY) ## Rotate and adjust the polygon back to 4 corner
     polyText = PolyRotateB.wkt
-    return polyText
+    
+    return (polyText,y1height,y2height)
 
 db="osi_social_db"
 dbuser="postgres"
@@ -175,7 +204,7 @@ for camera in CameraList:
     if cameraType == "FIXED":
 ##        viewingAngle = float(view_angle) * math.pi /180
         ## change the viewing angle to |\ <= viewing angle
-        polyText = CreateFixedFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
+        polyText,y1height,y2height = CreateFixedFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
         polyFHolder = []
         polyText=str(polyText).replace("POLYGON ((","")
         polyText=str(polyText).replace("))","")
@@ -186,22 +215,26 @@ for camera in CameraList:
             floatContent = [float(digit) for digit in splitstr]
             polyFHolder.append(floatContent)
 
-        print polyFHolder
+        ## for high oblique, change the far height to height of the camera mount.
+        if view_angle < 5:
+            y1height = cam_height
+
 
         # CheckFootprintExist = """Select count(uid) from office_footprint where uid = '%s';"""
         UpdateStatement = """update osi_camera
-                                set footprint_str = '%s'
+                                set footprint_str = '%s',
+                                y2height='%s',
+                                y1height ='%s'
                             where camera_uid = '%s';"""
 
-
-        cursor.execute(UpdateStatement % (str(polyFHolder),camera_UID))
+        cursor.execute(UpdateStatement % (str(polyFHolder),y1height,y2height,camera_UID))
         db.commit()
 
 
 
     elif cameraType == "DOME":
 
-        polyTextdOME = CreateDOMEFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
+        polyTextdOME,y1height,y2height = CreateDOMEFootprint(view_angle,sensorWidth,sensorHeight,focusLength,bearing,cameraX,cameraY)
         # polystarement = "ST_GeomFromText('"+polyTextdOME+"',3857)"
         polyHolder = []
         polyTextdOME=str(polyTextdOME).replace("POLYGON ((","")
@@ -213,16 +246,20 @@ for camera in CameraList:
             floatContent = [float(digit) for digit in splitstr]
             polyHolder.append(floatContent)
 
-        print polyHolder
+        ## for high oblique, change the far height to height of the camera mount.
+        if view_angle < 5:
+            y1height = cam_height
 
         # CheckFootprintExist = """Select count(uid) from office_footprint where uid = '%s';"""
         # InsertStatement = """insert into office_footprint(uid,geom) values('%s',%s)"""
         UpdateStatement = """update osi_camera
-                                set footprint_str = '%s'
+                                set footprint_str = '%s',
+                                y2height='%s',
+                                y1height ='%s'
                             where camera_uid = '%s';"""
-        print "UpdateStatement: "+str(UpdateStatement)
+        
 
-        cursor.execute(UpdateStatement % (str(polyHolder),camera_UID))
+        cursor.execute(UpdateStatement % (str(polyHolder),y1height,y2height,camera_UID))
 
         db.commit()
 
--- Footprint-master/camera_model.py
+++ Footprint-master/camera_model.py
@@ -1,31 +1,38 @@
-import numpy
+import numpy as np
 import psycopg2
+import ast
+import math
+
+from shapely import wkb
+from shapely.geometry import Polygon
+from shapely.affinity import rotate
+from shapely.wkt import dumps
 
 
 ## get the camera rotation matrix
 ## start with the camera model
 
-###########################################
-## x = K[R t] X
-## x: Image Coordinates: (u,v,1)
-## K: Intrinsic Matrix (3x3)
-## R: Rotation (3x3) 
-## t: Translation (3x1)
-## X: World Coordinates: (X,Y,Z,1)
-###########################################
+## input xy position of the screen and get the realworld coordinate in return
+def realworldXY(ScreenX,ScreenY,transformM):
+
+    ## need to include the shearing matrix into
+    a = transformM[0]
+    b = transformM[1]
+    c = transformM[2]
+    d = transformM[3]
+    e = transformM[4]
+    f = transformM[5]
+    g = transformM[6]
+    h = transformM[7]
+
+    realX = (a*ScreenX+b*ScreenY+c)/(g*ScreenX+h*ScreenY+1)
+    realY = (d*ScreenX+e*ScreenY+f)/(g*ScreenX+h*ScreenY+1)
 
 
-## Convert from older work done (Geolocation.js)
+    return (realX,realY)
 
-
-
-
-
-# Footprint = [
-# 		[x3, ((x4+x3)/2), x4, ((x3+x2)/2), ((x4+x3)/2),((x4+x1)/2),  x2,((x2+x1)/2),x1],
-#     [y2,y2,y2, ((y1+y2)/2), ((y1+y2)/2), ((y1+y2)/2), y1,y1,y1],[1,1,1,1,1,1,1,1,1]]
-
-def getCameraFOOTPrint(TVgrid):
+## Store the transformation
+def Store_transformM(transformM,cam_uid):
     db="osi_social_db"
     dbuser="postgres"
     dbpassword=""
@@ -37,28 +44,147 @@ def getCameraFOOTPrint(TVgrid):
 
     ## extract the floor plan to compare
     cursor = db.cursor()
-    cursor.execute("""select footprint_str,camera_uid from "osi_camera" ;""") ## <== get the floor from the floor plan
+    UpdateStatement = """update osi_camera
+                            set transformation_matrix = %s
+                            where camera_uid = %s;"""
+    act_sql = cursor.mogrify(UpdateStatement, (str(transformM), cam_uid))
+    print act_sql
+    cursor.execute(act_sql)
+    db.commit()
+
+
+## get camera footprint
+def getCameraFOOTPrint():
+    db="osi_social_db"
+    dbuser="postgres"
+    dbpassword=""
+    dbhost="192.168.8.12"
+    dbport="5432"
+
+    ## Load data from postgres
+    db = psycopg2.connect(database=db, user=dbuser, password=dbpassword, host=dbhost, port=dbport)
+
+    ## extract the floor plan to compare
+    cursor = db.cursor()
+    cursor.execute("""select * from "osi_camera";""") ## <== get the floor from the floor plan
     footprintA = cursor.fetchall()
-    for (fP,uid) in footprintA:
-        print fP
-        print uid
+    
+    del cursor
+    del db
+    return (footprintA)
 
 
+def reconstruct9Point(footprint2):
+    ## Coordinate in this order.
+    ## x4,y2 ------------ x3,y2
+    ##   |    (x4+x3)/2    |
+    ##    |              |
+    ## x2,y1 ---------- x1,y1
+    x4,y2 = footprint2[0]
+    x3,y2 = footprint2[1]
+    x1,y1 = footprint2[2]
+    x2,y1 = footprint2[3]
+    ## reconstruct the footprint from 4 points to 9 points
+    ReconstructFP = [[x4, ((x4+x3)/2), x3, ((x4+x2)/2), ((x4+x3)/2),((x3+x1)/2),x2,((x2+x1)/2),x1],
+    [y2,y2,y2, ((y1+y2)/2), ((y1+y2)/2), ((y1+y2)/2), y1,y1,y1]]
+    return (ReconstructFP,y1,y2,x4,x3)
 
 
+def transformationMatrix(footprint,TVgrid):
+    ## drop the element to 4 instead of 5 received in the array.
+    ## use the example from
+    ##  http://www.corrmap.com/features/homography_transformation.php
+    ##  this example works in python
+
+    footprint2 = footprint[0:4]
+    
+    ## x4,y2 ------------ x3,y2
+    ##   |    (x4+x3)/2    |
+    ##    |              |
+    ## x2,y1 ---------- x1,y1
+    ##  Big x and y in the sample
+    x4,y2 = footprint[0]
+    x3,y2 = footprint[1]
+    x1,y1 = footprint[2]
+    x2,y1 = footprint[3]
+ 
+
+    ## TVgrid POINTS
+    ## small x and y in the example
+    TX4,TX3,TX1,TX2 = TVgrid[0]
+    TY4,TY3,TY1,TY2 = TVgrid[1]
+
+    matrixA = np.array([[TX4,TY4,1,0,0,0,-TX4*x4,-TY4*x4],
+                [TX3,TY3,1,0,0,0,-TX3*x3,-TY3*x3],
+                [TX1,TY1,1,0,0,0,-TX1*x1,-TY1*x1],
+                [TX2,TY2,1,0,0,0,-TX2*x2,-TY2*x2],
+                [0,0,0,TX4,TY4,1,-TX4*y2,-TY4*y2],
+                [0,0,0,TX3,TY3,1,-TX3*y2,-TY3*y2],
+                [0,0,0,TX2,TY2,1,-TX2*y1,-TY2*y1],
+                [0,0,0,TX1,TY1,1,-TX1*y1,-TY1*y1]])
+
+    ## output matrix in real world coordinate system
+    O_matrix = np.array([x4,x3,x1,x2,y2,y2,y1,y1])
+    ## use python matrix solver to get the transformation matrix.
+    TMatrix = np.linalg.solve(matrixA,O_matrix)
+
+    return TMatrix
+    
+    
 
 if __name__ == '__main__':
     ## Screen resolution related
-    screen_resolution=[1080,720]
+    ## obtain the resolution from json
+    screen_resolution=[1920,1200]
     midX = screen_resolution[0]/2
     midY = screen_resolution[1]/2
 
-    TVgrid = [
-		[0,midX,screen_resolution[0], 0,midX,screen_resolution[0], 0,midX,screen_resolution[0]], 
-		[0,0,0,midY,midY,midY,screen_resolution[1],screen_resolution[1],screen_resolution[1]], 
-		[1,1,1,1,1,1,1,1,1]]
+    sX = 1194
+    sY = 879
 
-    getCameraFOOTPrint(TVgrid)
+    objectPixelH = 310
+
+    ## 4 points
+    TVgrid = [[0,screen_resolution[0],screen_resolution[0], 0],
+		[0,0,screen_resolution[1],screen_resolution[1]]]
+
+    for cameras in getCameraFOOTPrint():
+        
+        cam_uid = cameras[2]
+        cam_height = cameras[8]
+        camerapoint = wkb.loads(cameras[0],True)
+        camX = camerapoint.xy[0]
+        camY = camerapoint.xy[1]
+
+        ## Check if footprint exist
+        if cameras[20] is not None:
+            footprint =  ast.literal_eval(cameras[20])
+            # Get_view_angle = getCameraFOOTPrint()[1]
+
+            ## transformation matrix
+            transformM = transformationMatrix(footprint,TVgrid)
+            # print transformM
+
+            Store_transformM(transformM,cam_uid)
+
+            ## locationPosition (x,y)
+            a1 = [[sX,sY],[sX,sY+objectPixelH]]
+            CollectX = []
+            CollectY = []
+            for pos in a1:
+                xpos = pos[0]
+                ypos = pos[1]
+                realx,realy= realworldXY(xpos,ypos,transformM)
+                CollectX.append(realx)
+                CollectY.append(realy)
+
+            objDist = math.sqrt((CollectY[0]-CollectY[1])**2 + (CollectX[0]-CollectX[1])**2)
+            distanceToCamera = math.sqrt((CollectY[0]-camY)**2 + (CollectX[0]-camX)**2)
+            
+            objectHeight = (cam_height*objDist)/distanceToCamera
+            print objectHeight
+
+    # print "screen to real world=>" + str(resultRW)
 
 
 
